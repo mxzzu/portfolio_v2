01ab0c7df17d06689c773303e6de95238afc7367
Riccardo Mazzurana <riccardo.mazzurana@gmail.com>
2026-04-03 18:05:54 +0200

    added language section

---
+++ app/elements/languages/index.tsx
@@ -0,0 +1,62 @@
+import styles from "./languages.module.css";
+import Separator from "../../components/separator";
+import { languages } from "../../data/languages";
+
+export default function Header() {
+  return (
+    <section>
+      <div className="sectionTitle">
+        <h3>global correspondent</h3>
+        <Separator />
+      </div>
+
+      <div className={styles.languageGrid}>
+        <div className={styles.gridCol}>
+          <p>language</p>
+          <p>basic</p>
+          <p>intermediate</p>
+          <p>advanced</p>
+          <p className="hidden md:block">native language</p>
+        </div>
+        {languages.map((language, index) => (
+          <div key={index} className={styles.gridCol}>
+            <p>{language.language}</p>
+
+            <div className={styles.dotContainer}>
+              <div
+                className={`${styles.dot} ${language.level >= 1 && styles.dotFull}`}
+              />
+              <div
+                className={`${styles.dot} ${language.level >= 2 && styles.dotFull}`}
+              />
+            </div>
+
+            <div className={styles.dotContainer}>
+              <div
+                className={`${styles.dot} ${language.level >= 3 && styles.dotFull}`}
+              />
+              <div
+                className={`${styles.dot} ${language.level >= 4 && styles.dotFull}`}
+              />
+            </div>
+
+            <div className={styles.dotContainer}>
+              <div
+                className={`${styles.dot} ${language.level >= 5 && styles.dotFull}`}
+              />
+              <div
+                className={`${styles.dot} ${language.level >= 6 && styles.dotFull}`}
+              />
+            </div>
+
+            <div className={`hidden! ${styles.dotContainer} md:flex!`}>
+              <div
+                className={`${styles.dot} ${language.level >= 7 && styles.dotFull}`}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./elements/header";
+import Languages from "./elements/languages";
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <Languages />
     </main>
   );
 }
