16abec649fe7f38d057b77c3e79c0d2ec9cda698
Riccardo Mazzurana <riccardo.mazzurana@gmail.com>
2026-04-08 15:16:42 +0200

    changed icons

--- app/data/logos.ts
+++ app/data/logos.ts
@@ -1,25 +1,24 @@
 import { LogoItem } from "@/app/components/logoloop";
 import React from "react";
-import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss, SiPostman, SiPostgresql } from 'react-icons/si';
-import { TbBrandCSharp, TbBrandAndroid, TbBrandDocker, TbBrandPython, TbBrandMongodb, TbSql  } from "react-icons/tb";
+import Image from "next/image" 
 
 export const web: LogoItem[] = [
-    { node: React.createElement(SiReact), title: "React", href: "https://react.dev" },
-    { node: React.createElement(SiNextdotjs), title: "Next.js", href: "https://nextjs.org" },
-    { node: React.createElement(SiTypescript), title: "TypeScript", href: "https://www.typescriptlang.org" },
-    { node: React.createElement(SiTailwindcss), title: "Tailwind CSS", href: "https://tailwindcss.com" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/react/mono.svg", alt:"React", width: 48, height: 48 }), title: "React", href: "https://react.dev" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/nextdotjs/mono.svg", alt:"NextJS", width: 48, height: 48 }), title: "Next.js", href: "https://nextjs.org" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/typescript/mono.svg", alt:"TypeScript", width: 48, height: 48 }), title: "TypeScript", href: "https://www.typescriptlang.org" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/tailwind-css/mono.svg", alt:"TailwindCSS", width: 48, height: 48 }), title: "Tailwind CSS", href: "https://tailwindcss.com" },
 ];
 
 export const software: LogoItem[] = [
-    { node: React.createElement(TbBrandAndroid), title: "Android", href: "https://www.android.com/" },
-    { node: React.createElement(TbBrandCSharp), title: "C#", href: "https://react.dev" },
-    { node: React.createElement(TbBrandDocker), title: "Docker", href: "https://react.dev" },
-    { node: React.createElement(TbBrandPython), title: "Python", href: "https://react.dev" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/android/mono.svg", alt:"Android", width: 48, height: 48 }), title: "Android", href: "https://www.android.com/" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/c/mono.svg", alt:"C", width: 48, height: 48 }), title: "C", href: "https://www.c-language.org/" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/docker/mono.svg", alt:"Docker", width: 48, height: 48 }), title: "Docker", href: "https://docker.com" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/python/mono.svg", alt:"Python", width: 48, height: 48 }), title: "Python", href: "https://python.org" },
 ];
 
 export const database: LogoItem[] = [
-    { node: React.createElement(TbBrandMongodb), title: "MongoDB", href: "https://www.android.com/" },
-    { node: React.createElement(TbSql), title: "SQL", href: "https://react.dev" },
-    { node: React.createElement(SiPostman), title: "Postman", href: "https://react.dev" },
-    { node: React.createElement(SiPostgresql), title: "Postgres", href: "https://react.dev" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/mongodb/mono.svg", alt:"MongoDB", width: 48, height: 48 }), title: "MongoDB", href: "https://www.mongodb.com/" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/mysql/mono.svg", alt:"MySQL", width: 64, height: 64 }), title: "SQL", href: "https://mysql.com" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/postman/mono.svg", alt:"Postman", width: 48, height: 48 }), title: "Postman", href: "https://postman.com" },
+    { node: React.createElement(Image, { src: "https://thesvg.org/icons/postgresql/mono.svg", alt:"Postgres", width: 48, height: 48 }), title: "Postgres", href: "https://postgresql.org" },
 ];
